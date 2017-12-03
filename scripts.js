2e0d02a4a1502bd7b5c85409713e7cf2f06ca570
Magda Balun <magi@op.pl>
2017-12-03 23:33:55 +0100

    Add selection buttons in index.html and give events to the button in .js file

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
+//give the event to the start button
 var newGameButton = document.getElementById('js-newGameButton');
 
 newGameButton.addEventListener('click', newGame);
+
+//give the event to selection buttons
+var pickRock = document.getElementById('js-playerPick_rock'),
+    pickPaper = document.getElementById('js-playerPick_paper'),
+    pickScissors = document.getElementById('js-playerPick_scissors');
+
+pickRock.addEventListener('click', function() { playerPick('rock') });
+pickPaper.addEventListener('click', function() { playerPick('paper') });
+pickScissors.addEventListener('click', function() { playerPick('scissors') });
