bee55ac448639a7d2931ce35689911b396461c77
Magda Balun <magi@op.pl>
2017-12-04 00:47:19 +0100

    Add start the game code to .js file

--- scripts.js
+++ scripts.js
@@ -43,3 +43,20 @@ function setGameElements() {
   	}
 }
 setGameElements();
+
+//start the game
+var playerPointsElem = document.getElementById('js-playerPoints'),
+    playerNameElem = document.getElementById('js-playerName'),
+    computerPointsElem = document.getElementById('js-computerPoints');
+
+function newGame() {
+	player.name = prompt('Please enter your name', 'player name');
+	if (player.name) {
+    	player.score = computer.score = 0;
+    	gameState = 'started';
+    	setGameElements();
+
+    	playerNameElem.innerHTML = player.name;
+    	// setGamePoints(); // This function has not been created yet
+  	}
+}
